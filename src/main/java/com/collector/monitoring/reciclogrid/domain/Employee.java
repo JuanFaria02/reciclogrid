d5cb7d714b0a5f5c6fd0953dff4a88568d4adea1
juan <juan.faria@tecsinapse.com.br>
2025-02-12 20:17:09 -0300

    bugfix: Refatorado employee para satisfazer a autenticação de usuários
    - Criado coluna para o número do documento
    - Criado enum para o tipo do usuário
    - Criado implementação do UserDetail para satisfazer a configuração de autenticação

--- src/main/java/com/collector/monitoring/reciclogrid/domain/Employee.java
+++ src/main/java/com/collector/monitoring/reciclogrid/domain/Employee.java
@@ -1,15 +1,22 @@
 package com.collector.monitoring.reciclogrid.domain;
 
+import com.collector.monitoring.reciclogrid.domain.enums.UserType;
 import jakarta.persistence.*;
 import jdk.jfr.Timestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
 @Table(name = "employee")
-public class Employee {
+public class Employee implements UserDetails, Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -22,6 +29,16 @@ public class Employee {
 
     @Column(nullable = false)
     private String phone;
+
+    @Column(nullable = false, name = "type")
+    @Enumerated(EnumType.STRING)
+    private UserType type;
+
+    @Column(name = "document_number", unique = true)
+    private String documentNumber;
+
+    @Column(nullable = false, columnDefinition = "text")
+    private String password;
 
     @Column(nullable = false)
     private boolean active = true;
@@ -40,12 +57,15 @@ public class Employee {
     public Employee() {
     }
 
-    public Employee(Long id, String name, String email, String phone, Company company) {
+    public Employee(Long id, String name, String email, String phone, Company company, UserType type, String documentNumber, boolean active) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.phone = phone;
         this.company = company;
+        this.type = type;
+        this.documentNumber = documentNumber;
+        this.active = active;
     }
 
     public Long getId() {
@@ -98,5 +118,44 @@ public class Employee {
 
     public LocalDateTime getCreatedAt() {
         return createdAt;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.type == UserType.ADMIN) {
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        }
+
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return active;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return active;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
     }
 }

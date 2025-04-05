2d927cc7011e85e8da5e18d21900a28a1503bbbd
juan <juan.faria@tecsinapse.com.br>
2025-04-04 21:20:22 -0300

    feat: Refatorar Código

--- src/main/java/com/collector/monitoring/reciclogrid/repository/MicrocontrollerRepository.java
+++ src/main/java/com/collector/monitoring/reciclogrid/repository/MicrocontrollerRepository.java
@@ -3,7 +3,9 @@ package com.collector.monitoring.reciclogrid.repository;
 import com.collector.monitoring.reciclogrid.domain.Collector;
 import com.collector.monitoring.reciclogrid.domain.Microcontroller;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MicrocontrollerRepository extends JpaRepository<Microcontroller, Long> {
     Microcontroller findByCollector(Collector collector);
     Microcontroller findByName(String name);

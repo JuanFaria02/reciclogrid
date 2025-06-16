e8ba07f5067cf7b266ce1507ba4d13859362d749
juan <juan.faria@tecsinapse.com.br>
2025-06-16 18:52:40 -0300

    feat: Acrescentado quantidade de coletores ao usuário

--- src/main/java/com/collector/monitoring/reciclogrid/domain/dto/EmployeeDTO.java
+++ src/main/java/com/collector/monitoring/reciclogrid/domain/dto/EmployeeDTO.java
@@ -2,5 +2,5 @@ package com.collector.monitoring.reciclogrid.domain.dto;
 
 import com.collector.monitoring.reciclogrid.domain.enums.UserType;
 
-public record EmployeeDTO(Long id, String name, String email, String phone, UserType type, Integer qtdCollectos, String documentNumber, String position, CompanyDTO company) {
+public record EmployeeDTO(Long id, String name, String email, String phone, UserType type, Integer qtdCollectors, String documentNumber, String position, CompanyDTO company) {
 }
